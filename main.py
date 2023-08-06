e536e0692edbacb75b6e2bf6d4c987f329d4672a
darshannv <darshanvasu3048@gmail.com>
2023-08-06 20:07:20 +0530

    load excel

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas
+import pandas as pd
+import glob
+
+filepaths = glob.glob("invoices/*.xlsx")
+
+for filepath in filepaths:
+    df = pandas.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
